55a54424784128fabbe76a7b2541d978d7d61040
RiscoCountyMoe <j.time@gmx.de>
2023-05-07 15:56:22 +0200

    added more tests

--- test/test_database.py
+++ test/test_database.py
@@ -4,7 +4,7 @@ from data.database import Database
 
 
 class TestDatabase(unittest.TestCase):
-    DB_NAME = "test.db"
+    DB_NAME = "DLMDWPMP01.db"
 
     def setUp(self):
         self.db = Database(TestDatabase.DB_NAME)
@@ -19,7 +19,7 @@ class TestDatabase(unittest.TestCase):
 
     def test_create_table(self):
         table_name = "test_table"
-        sql = f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, column TEXT)"
+        sql = f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, column TEXT)"
 
         self.db.create_table(table_name, sql)
 
@@ -32,3 +32,44 @@ class TestDatabase(unittest.TestCase):
             result = cursor.fetchone()
 
         self.assertIsNotNone(result)
+
+    def test_load_data_to_table(self):
+        table_name = "test_table"
+        sql = f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, function TEXT)"
+
+        self.db.create_table(table_name, sql)
+
+        data = [(1, "y1"), (2, "y2"), (3, "y3")]
+
+        self.db.load_data_to_table(
+            table_name, f"INSERT INTO {table_name} VALUES (?, ?)", data
+        )
+
+        with sqlite3.connect(TestDatabase.DB_NAME) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            result = cursor.fetchall()
+
+        expected = [(1, "y1"), (2, "y2"), (3, "y3")]
+
+        self.assertEqual(result, expected)
+
+    def test_load_table_to_dataframe(self):
+        table_name = "test_table"
+        sql = f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, function TEXT)"
+
+        self.db.create_table(table_name, sql)
+
+        data = [(1, "y1"), (2, "y2"), (3, "y3")]
+
+        self.db.load_data_to_table(
+            table_name, f"INSERT INTO {table_name} VALUES (?, ?)", data
+        )
+
+        df = self.db.load_table_to_dataframe(table_name)
+
+        expected_columns = ["id", "function"]
+        expected_values = [(1, "y1"), (2, "y2"), (3, "y3")]
+
+        self.assertEqual(list(df.columns), expected_columns)
+        self.assertEqual(list(df.itertuples(index=False, name=None)), expected_values)
